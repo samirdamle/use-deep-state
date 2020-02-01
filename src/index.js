7e4b092514902016b7a7a4d50691a63da2ea0ca0
Samir Damle <damlemail@gmail.com>
2020-02-01 00:01:30 -0500

    Added deepValue method to get and set a value deep into an object

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import * as React from 'react'
 
 const clone = obj => JSON.parse(JSON.stringify(obj))
-const getDeepValue = (obj, pathArray) => pathArray.reduce((item, prop) => (item && item[prop] ? item[prop] : null), obj)
-const setDeepValue = (obj, pathArray, value) => {
-    const node = getDeepValue(obj, pathArray.slice(0, -1))
-    if (node && typeof node === 'object') {
-        node[pathArray.slice(-1)] = value
-        return obj
+
+const deepValue = (obj, pathArray, value) => {
+    const get = (obj, pathArray) => pathArray.reduce((item, prop) => (item && item[prop] ? item[prop] : null), obj)
+    if (value === undefined) {
+        return get(obj, pathArray)
     } else {
-        return null
+        const node = get(obj, pathArray.slice(0, -1))
+        if (node && typeof node === 'object') {
+            node[pathArray.slice(-1)] = value
+            return obj
+        } else {
+            return null
+        }
     }
 }
 
@@ -16,8 +21,8 @@ const useDeepState = (initialState = {}) => {
     let [state, _setState] = React.useState(initialState)
     const setState = (updates = {}, value = null) => {
         if (Array.isArray(updates) && updates.length > 0) {
-            const pathArray = updates
-            const newState = setDeepValue(clone(state), pathArray, value)
+            const pathArray = [...updates]
+            const newState = deepValue(clone(state), pathArray, value)
             if (newState) _setState(newState)
         } else if (typeof updates === 'object') {
             _setState(prevState => {
